d30df28845951293300a330663bf94dc6ec70426
Dheer Soni <dheersoni13@gmail.com>
2025-11-27 09:06:01 +0530

    27/11--0902

--- flake.nix
+++ flake.nix
@@ -3,24 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, ... }: {
     nixosConfigurations.The-Machine = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
           modules = [
             ./configuration.nix
-            home-manager.nixosModules.home-manager
-                     {
-                        home-manager = {
-                            useGlobalPkgs = true;
-                            useUserPackages = true;
-                            users.dheer = import ./home.nix;
-                            backupFileExtension= "backup";
-                          };
-                       }
                      ];
                    };
                 };
