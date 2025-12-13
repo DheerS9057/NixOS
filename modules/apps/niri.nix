38e864f46b9e17f920fbdf505a606e1df23dedfc
Dheer Soni <dheersoni13@gmail.com>
2025-12-13 21:20:49 +0530

    13/12--2118

--- modules/apps/niri.nix
+++ modules/apps/niri.nix
@@ -1,8 +1,21 @@
 { pkgs, inputs, ... }:
+let
+  quickshellWithQt6 = pkgs.quickshell.overrideAttrs (old: {
+    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [
+      pkgs.qt6.wrapQtAppsHook
+    ];
 
+    buildInputs = (old.buildInputs or []) ++ [
+      pkgs.qt6.qtbase
+      pkgs.qt6.qtmultimedia
+      pkgs.qt6.qtdeclarative
+      pkgs.qt6.qtsvg
+    ];
+  });
+in
 {
   environment.systemPackages = with pkgs; [
-    quickshell
+    quickshellWithQt6
     swaybg
     kdePackages.polkit-kde-agent-1
     xwayland-satellite
