b8ef6fd2516e48209024b7095cb209aa1cec7bcb
Dheer Soni <dheersoni13@gmail.com>
2025-12-13 15:56:27 +0530

    13/12--1549

--- flake.nix
+++ flake.nix
@@ -18,6 +18,10 @@
   outputs = inputs@{ self, nixpkgs, ... }: {
     nixosConfigurations.The-Machine = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+          specialArgs = {
+        inherit inputs;
+      };
+
           modules = [
             ./configuration.nix
                      ];
