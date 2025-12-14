41b269f26e75f05c9568e383a096efe49a590cd9
Dheer Soni <dheersoni13@gmail.com>
2025-12-14 15:32:19 +0530

    14/12--1529

--- modules/apps/ui.nix
+++ modules/apps/ui.nix
@@ -2,45 +2,46 @@
 
 {
   environment.systemPackages = with pkgs; [
-      nwg-look
-      adwaita-icon-theme
-      gnome-themes-extra
-      rose-pine-hyprcursor
-      font-awesome
-      qadwaitadecorations
-      bibata-cursors
-      gruvbox-plus-icons
-      libsForQt5.qt5.qtsvg
-      libsForQt5.qt5.qtimageformats
-      libsForQt5.qt5.qtmultimedia
-      xfce.tumbler
-      lxappearance
+    nwg-look
+    adwaita-icon-theme
+    gnome-themes-extra
+    rose-pine-hyprcursor
+    font-awesome
+    qadwaitadecorations
+    bibata-cursors
+    gruvbox-plus-icons
+    libsForQt5.qt5.qtsvg
+    libsForQt5.qt5.qtimageformats
+    libsForQt5.qt5.qtmultimedia
+    xfce.tumbler
+    lxappearance
   ];
-  fonts.packages = with pkgs; [
-  noto-fonts
-  noto-fonts-cjk-sans
-  noto-fonts-color-emoji
-  liberation_ttf
-  dejavu_fonts
-  freefont_ttf
-  corefonts
-  vista-fonts
-];
 
-fonts.fontconfig = {
-  enable = true;
-  antialias = true;
-  hinting.enable = true;
-  hinting.style = "full";
-  subpixel.rgba = "rgb";
-};
+  fonts = {
+    fontconfig = {
+      enable = true;
+      antialias = true;
+      hinting.enable = true;
+      hinting.style = "full";
+      subpixel.rgba = "rgb";
+    };
 
-fonts.packages = [
-  (pkgs.runCommand "tabler-icons-font" {} ''
-    mkdir -p $out/share/fonts/truetype
-    cp ${./path/to/tabler-icons.ttf} $out/share/fonts/truetype/
-  '')
-];
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      liberation_ttf
+      dejavu_fonts
+      freefont_ttf
+      corefonts
+      vista-fonts
+      font-awesome
 
-
+      (runCommand "tabler-icons-font" {} ''
+        mkdir -p $out/share/fonts/truetype
+        cp ${./path/to/tabler-icons.ttf} $out/share/fonts/truetype/
+      '')
+    ];
+  };
 }
+
